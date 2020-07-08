3c3a7652bbf6099d96523a75ebb0a8e49665cca4
jose <jota.rf.88@gmail.com>
2020-07-08 19:13:05 -0400

    mas inserts

---
+++ Scripts SQL/sustancias_prohibidas.sql
@@ -0,0 +1,125 @@
+/*
+    INSERTS de la base de datos del proyecto de perfumes
+    Grupo #2
+    Seccion 002
+    Ricardo Salvatorelli (C.I.: 26.967.602)
+    Jose Manuel Ramirez (C.I.: 26.902.002)
+    David Zacarias (C.I.: 27.588.099)
+*/
+
+
+/* INSERT de las sustancias prohibidas*/
+INSERT INTO rdj_sustancias_prohibidas (num_cas,nombre) VALUES
+(103640,'Bromostyrene');
+(103797,'Phenyl acetone');
+(104278,'α-Methyl anisylidene acetone');
+(108883,'Toluene');
+(109864,'Ethylene glycol monomethyl ether');
+(110496,'Ethylene glycol monomethyl acetate');
+(110805,'Ethylene glycol monoethyl ether');
+(111159,'Ethylene glycol monoethyl acetate');
+(111284,'2-Hexadien-1-ol');
+(17102646,'4-Hexadien-1-ol');
+(116665,'Musk moskene');
+(122394,'Diphenylamine');
+(122576,'Benzylidene acetone');
+(26266773,'Hydroabietyl alcohol');
+(1333897,'Dihydroabietyl alcohol');
+(13706860,'Acetyl isovaleryl');
+(140294,'Benzyl cyanide');
+(140885,'Ethyl acrylate');
+(141059,'Diethyl maleate');
+(141106,'Pseudoionone');
+(145391,'Musk tibetene');
+(150765,'Hydroquinone monomethyl ether');
+(17874349,'4,6-Dimethyl-8-tert-butylcoumarin');
+(18318837,'trans-2-Hexenal dimethyl acetal');
+(18485386,'2,4-Dodecadien-1-ol, (2E, 4E)');
+(18829555,'trans-2-Heptenal');
+(2445832,'7-Methylcoumarin');
+(25564221,'Amylcyclopentenone');
+(25677401,'2-Pentylidene cyclohexanone');
+(26651967,'Pseudo methylionones');
+(72968253,'Pseudo methylionones');
+(1117415,'Pseudo methylionones');
+(33204749,'Carvone oxide');
+(34131992,'6-Isopropyl-2-decalol');
+(3591422,'2,2-Dichloro-1-methylcyclopropylbenzene');
+(40607485,'3,7-Dimethyl-2-octen-1-ol');
+(4173448,'Cinnamylidene acetone');
+(4756198,'Cyclamen alcohol');
+(494406,'cis-and trans-Asarone');
+(2883989,'cis-and trans-Asarone');
+(5273869,'cis-and trans-Asarone');
+(5146667,'Geranyl nitrile');
+(5585397,'Geranyl nitrile');
+(31983274,'Geranyl nitrile');
+(531599,'7-Methoxycoumarin');
+(53153665,'3-Methyl-2(3)-nonenenitrile');
+(5406122,'p-Methylhydrocinnamic aldehyde');
+(54814641,'Massoia lactone');
+(51154962,'Massoia lactone');
+(57067,'Allyl isothiocyanate');
+(617549,'Dimethyl citraconate');
+(622628,'Hydroquinone monoethyl ether');
+(62265990,'Musk KS');
+(623154,'Furfurylidene acetone');
+(623438,'Methyl crotonate');
+(6248200,'2,4-Dihydroxy-3-methylbenzaldehyde');
+(63697530,'Musk α');
+(67746309,'trans-2-Hexenal diethyl acetal');
+(68916529,'Fig leaf absolute');
+(90028743,'Fig leaf absolute');
+(700823,'Hexahydrocoumarin');
+(71432,'Benzene');
+(73157434,'Allyl heptine carbonate');
+(76299,'3-Bromo-1,7,7-trimethylbicyclo[2.2.1]heptane-2-one');
+(764409,'2,4-Dienals');
+(142836,'2,4-Dienals');
+(80466348,'2,4-Dienals');
+(78591,'Isophorone');
+(80546,'p-tert-Butyl-α-methylhydrocinnamic aldehyde (p-BMHCA)');
+(80626,'Methyl methacrylate');
+(8001885,'Birch wood pyrolysate');
+(68917500,'Birch wood pyrolysate');
+(84012157,'Birch wood pyrolysate');
+(8006993,'Chenopodium oil');
+(8024111,'Chenopodium oil');
+(89997477,'Chenopodium oil');
+(8007009,'Peru balsam');
+(8013103,'Cade oil');
+(90046029,'Cade oil');
+(8022819,'Boldo oil');
+(84649967,'Boldo oil');
+(8023889,'Costus root oil, absolute and concrete');
+(90106551,'Costus root oil, absolute and concrete');
+(8024122,'Verbena oil and absolute (Lippia citriodora Kunth.)');
+(85116638,'Verbena oil and absolute (Lippia citriodora Kunth.)');
+(8046193,'Styrax');
+(8024019,'Styrax');
+(94891277,'Styrax');
+(8050097,'Colophony');
+(81152,'Musk xylene');
+(83669,'Musk ambrette');
+(84012204,'Alantroot oil');
+(97676352,'Alantroot oil');
+(84961580,'Santolina oil');
+(85085263,'Massoia bark oil');
+(87058,'4-Methyl-7-ethoxycoumarin');
+(88299,'Acetyl ethyl tetramethyl tetralin (AETT)');
+(91225,'Quinoline');
+(92488,'6-Methylcoumarin');
+(93992,'Phenyl benzoate');
+(94597120,'Safrole, Isosafrole and Dihydrosafrole');
+(58194586,'Safrole, Isosafrole and Dihydrosafrole');
+(943884,'Anisylidene acetone');
+(98000,'Furfuryl alcohol');
+(98544,'p-tert-Butylphenol');
+(98953,'Nitrobenzene');
+(10031922,'Esters of 2-Nonynoic acid (except Methyl octine carbonate)');
+(10484329,'Esters of 2-Octynoic acid (except Methyl heptine carbonate)');
+(10519207,'Esters of 2-Octynoic acid (except Methyl heptine carbonate)');
+(8024008,'Savin oil');
+(90046041,'Savin oil');
+(90131434,'Tagetes oil and absolute');
+(8016840,'Tagetes oil and absolute');
